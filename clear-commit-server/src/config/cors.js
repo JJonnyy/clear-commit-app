d26888702f2e559480818996af94e756e244b86b
Yevhen Vlasenko <igusarr@gmail.com>
2025-02-04 01:39:09 +0200

    cors

--- clear-commit-server/src/config/cors.js
+++ clear-commit-server/src/config/cors.js
@@ -1,7 +1,14 @@
 export const corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    exposedHeaders: ['Content-Type'],
+    origin: process.env.NODE_ENV === 'production'
+        ? process.env.VITE_CLIENT_URL
+        : 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Accept'
+    ],
     credentials: true,
+    optionsSuccessStatus: 200
 }
