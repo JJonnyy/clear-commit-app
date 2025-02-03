40a9be23726635833698919d61cb4a96ddcab379
Yevhen Vlasenko <igusarr@gmail.com>
2025-02-04 01:36:05 +0200

    port change

--- clear-commit-server/src/config/config.js
+++ clear-commit-server/src/config/config.js
@@ -2,6 +2,6 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PUBLIC_PORT = process.env.PUBLIC_PORT;
+export const PUBLIC_PORT = process.env.PUBLIC_PORT || 8000;
 export const WEB_TOKEN_SECRET_KEY = process.env.WEB_TOKEN_SECRET_KEY;
 export const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY;
